cfec681ee99a0e44029e24bd618a163aa6878a3b
Aiden <aiden.marques94@gmail.com>
2020-11-17 10:50:48 -0500

    commit tree 0e51beb72a7e2d2f887c790812154bc9a6965712

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(stuff)
+i = 0
+collects = []
+while i < stuff.length
+    collects << yield(stuff[i])
+    i+= 1
+end 
+collects
+end 
 
-
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
